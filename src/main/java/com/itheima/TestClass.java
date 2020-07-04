d25c8ec5c36a65093df0e555bd051e47db372de2
zhuchenzhong <648847873@qq.com>
2020-07-04 11:22:56 +0800

    提交本地仓库

---
+++ src/main/java/com/itheima/TestClass.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+/**
+ * @program: My_ssm_parent
+ * @description:
+ * @author: ju F
+ * @create: 2020-07-04 11:14
+ */
+public class TestClass {
+    private String name;
+    private Integer age;
+}
